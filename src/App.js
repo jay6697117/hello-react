fd088a57e3b234e232010c971fd17bce65b5d1e0
steve zhang <871506263@qq.com>
2022-03-25 01:09:05 +0800

    提交代码于: 2022-03-25 01:09:05

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import logo from './assets/images/logo.svg';
 import './assets/style/App.css';
 
 class Title extends Component {
+  handleTitleClick(val, e) {
+    console.log('val:', val);
+    console.log('this:', this);
+    console.log('e:', e);
+    console.log('输出:', e.target.innerHTML);
+  }
   render() {
     return (
       <div className='title'>
-        <h1 className='title-text'>React小书</h1>
+        <h1 onClick={this.handleTitleClick.bind(this, 'hello')} className='title-text'>
+          React小书
+        </h1>
         <img src={logo} className='logo' alt='logo' />
       </div>
     );
@@ -15,7 +23,6 @@ class Title extends Component {
 
 class Header extends Component {
   renderBl(bool, dom1, dom2) {
-    console.log('renderBl this:', this);
     return bool ? dom1 : dom2;
   }
   render() {
@@ -27,7 +34,6 @@ class Header extends Component {
     const bl = Math.floor(Math.random() * 2);
     const btn1 = <div className='btn'>{bl ? '真的' : null}</div>;
     const btn2 = <div className='btn'>{!bl ? '假的' : null}</div>;
-    console.log('render this:', this);
     return (
       <div className='header'>
         <Title />
