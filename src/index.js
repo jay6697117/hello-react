27b91a4bfa1fdbda6e61e5f6aea8e53121c174c3
steve zhang <871506263@qq.com>
2022-04-15 15:17:11 +0800

    提交代码于: 2022-04-15 15:17:11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import './assets/style/index.css';
-import App from './CommentApp';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
