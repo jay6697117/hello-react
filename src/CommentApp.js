e962da4e850deea16514e9a81dbc0521b7d79f40
steve zhang <871506263@qq.com>
2022-04-18 10:21:14 +0800

    提交代码于: 2022-04-18 10:21:14

--- src/CommentApp.js
+++ src/CommentApp.js
@@ -7,7 +7,16 @@ class CommentApp extends Component {
   constructor() {
     super();
     this.state = {
-      comments: []
+      comments: [
+        {
+          username: '李小龙',
+          content: '哈哈哈哈'
+        },
+        {
+          username: '陈小春',
+          content: '洗洗休息累了'
+        }
+      ]
     };
   }
   handleSubmitComment(param) {
@@ -16,7 +25,7 @@ class CommentApp extends Component {
     if (!param.username) return alert('用户名不能为空!');
     if (!param.content) return alert('评论内容不能为空!');
 
-    const commentsTemp = [...this.state.comments];//浅拷贝
+    const commentsTemp = [...this.state.comments]; //浅拷贝
     commentsTemp.push(param);
     console.log('handleSubmitComment this.state.comments:', this.state.comments);
     console.log('handleSubmitComment commentsTemp:', commentsTemp);
