c09e841396dce85838d5b5a1fa8f15e2f5dba5ba
steve zhang <871506263@qq.com>
2022-04-18 14:48:17 +0800

    提交代码于: 2022-04-18 14:48:17

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './assets/style/app.css';
+
+class Header extends Component {
+  constructor() {
+    super();
+    console.log('constructor 1');
+  }
+  componentWillMount() {
+    console.log('component will mount 2');
+  }
+  componentDidMount() {
+    console.log('component did mount 4');
+  }
+  componentWillUnmount() {
+    console.log('component will unmount 5');
+  }
+  render() {
+    console.log('render 3');
+    return (
+      <div>
+        <h1 className='title'>React 小书</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './assets/style/index.css';
-import App from './app';
+import Header from './Header';
 
+class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isShowHeader: true
+    };
+  }
+  handleShowOrHide() {
+    this.setState({
+      isShowHeader: !this.state.isShowHeader
+    });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.isShowHeader ? <Header /> : null}
+        <button onClick={this.handleShowOrHide.bind(this)}>显示或者隐藏标题</button>
+      </div>
+    );
+  }
+}
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+// ReactDOM.render(React.createElement(Header, null), document.getElementById('root'));
+ReactDOM.render(<Index />, document.getElementById('root'));
