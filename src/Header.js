c7a8dde4bca26856b8a405463bd4f717c1872002
steve zhang <871506263@qq.com>
2022-05-13 17:48:05 +0800

    提交代码于: 2022-05-13 17:48:05

--- src/Header.js
+++ src/Header.js
@@ -7,11 +7,26 @@ class Header extends Component {
     console.log('constructor 1');
     this.state = {
       dateNow: new Date().Format('yyyy-MM-dd hh:mm:ss'),
-      timer: null
+      timer: null,
+      result: null
     };
   }
-  componentWillMount() {
+  async componentWillMount() {
     console.log('component will mount 2');
+    let result = null;
+    let response = await fetch('https://json-server-demo-rho.vercel.app/companies');
+    if (response.ok) {
+      // 如果 HTTP 状态码为 200-299
+      // 获取 response body（此方法会在下面解释）
+      result = await response.json();
+    } else {
+      alert('HTTP-Error: ' + response.status);
+    }
+    console.log('result', result);
+    this.setState({
+      result
+    });
+
     //异步处理
     this.mySetInterval(() => {
       this.setState({
@@ -24,7 +39,7 @@ class Header extends Component {
   }
   componentWillUnmount() {
     console.log('component will unmount 5');
-    clearTimeout(this.state.timer);//清除定时器
+    clearTimeout(this.state.timer); //清除定时器
     //设置timer 为 null
     this.setState({
       timer: null
@@ -42,11 +57,22 @@ class Header extends Component {
     };
     rec(callback, delay);
   }
+  listHandle(arr) {
+    if (arr && arr.length > 0) {
+      const listArr = arr.map(item => {
+        return <div key={item.id}>{item.name}</div>;
+      });
+      return listArr;
+    } else {
+      return null;
+    }
+  }
   render() {
-    // console.log('render 3');
+    console.log('render 3');
     return (
       <div className='header'>
         <h1 className='title'>当前时间:{this.state.dateNow}</h1>
+        <div>{this.listHandle(this.state.result)}</div>
       </div>
     );
   }
