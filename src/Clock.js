19eb12002c01908fd196636f383e9e4d2de8f54b
steve zhang <871506263@qq.com>
2022-05-14 23:05:41 +0800

    提交代码于: 2022-05-14 23:05:41

--- src/Clock.js
+++ src/Clock.js
@@ -4,11 +4,13 @@ class Clock extends Component {
   constructor() {
     super();
     console.log('Clock constructor 1');
+    // 一般会把组件的 state 的初始化工作放在 constructor 里面去做
     this.state = {
       dateNow: new Date().Format('yyyy-MM-dd hh:mm:ss'),
       timer: null
     };
   }
+  // 在 componentWillMount 进行组件的启动工作，例如 Ajax 数据拉取、定时器的启动
   componentWillMount() {
     console.log('Clock component will mount 2');
 
@@ -19,9 +21,11 @@ class Clock extends Component {
       });
     }, 1000);
   }
+  // 一般来说，有些组件的启动工作是依赖 DOM 的，例如动画的启动，而 componentWillMount 的时候组件还没挂载完成，所以没法进行这些启动工作，这时候就可以把这些操作放在 componentDidMount 当中
   componentDidMount() {
     console.log('Clock component did mount 4');
   }
+  // 组件从页面上销毁的时候，有时候需要一些数据的清理，例如定时器的清理，就会放在 componentWillUnmount 里面去做
   componentWillUnmount() {
     console.log('Clock component will unmount 5');
     clearTimeout(this.state.timer); //清除定时器
