d41df476d83c8a705a9be1043463afe3bc9c1e84
steve zhang <871506263@qq.com>
2022-05-18 00:35:03 +0800

    提交代码于: 2022-05-18 00:35:03

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# 进度: Lesson 14 实战分析：评论功能（一）
-- https://www.bookstack.cn/read/react-naive-book/caeba5be343a774e.md
+# 进度
 
 ### `npm start`
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
+import './assets/style/common.css';
 import './assets/style/index.css';
 import App from './App';
 
