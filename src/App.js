36f053ca7c16f14576e51b27198b3687678c5bbe
steve zhang <871506263@qq.com>
2022-05-20 21:00:27 +0800

    提交代码于: 2022-05-20 21:00:27

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './assets/style/app.css';
-// import Card from './Card';
-// import Layout from './Layout';
+import Card from './Card';
+import Layout from './Layout';
+import Style from './Style';
 
-/*
 class App extends Component {
   render() {
     return (
@@ -44,64 +44,8 @@ class App extends Component {
             <h1>我是内容啦啦啦啦啦啦</h1>
           </div>
         </Layout>
-      </div>
-    );
-  }
-}
-*/
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      content: '<h1 style="color:blue;background-color:red;padding:10px 0;">hello world</h1>',
-      commonStyle: { color: 'white', fontSize: '30px', textAlign: 'center', padding: '10px 0' },
-      backgroundColor: 'green',
-      opacity: 0
-    };
-  }
-  componentDidMount() {
-    setInterval(() => {
-      const opacity = this.state.opacity + 0.01;
-      if (opacity > 1) {
-        this.setState({
-          opacity: 1
-        });
-        return;
-      }
-      this.setState({
-        opacity
-      });
-    }, 20);
-  }
-  componentWillUnmount() {
-    this.setState = () => false;
-  }
-  handleClick() {
-    const colorArr = ['green', 'purple', 'blue', 'red', 'cyan'];
-    const random = Math.floor(Math.random() * 5); // 0-4
-    console.log('random', random);
-    this.setState({
-      backgroundColor: colorArr[random],
-      opacity: 0
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <button style={{ marginBottom: '20px', padding: '2px 10px' }} onClick={this.handleClick.bind(this)}>
-          点击
-        </button>
-        <div style={{ marginBottom: '20px' }} dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
-        <div
-          style={{
-            ...this.state.commonStyle,
-            backgroundColor: this.state.backgroundColor,
-            opacity: this.state.opacity
-          }}>
-          大家好啊
-        </div>
+        <div style={{ height: '1px', margin: '20px 0', backgroundColor: '#ccc' }}></div>
+        <Style />
       </div>
     );
   }
---
+++ src/Style.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+class Style extends Component {
+  constructor() {
+    super();
+    this.state = {
+      content: '<h1 style="color:blue;background-color:red;padding:10px 0;">hello world</h1>',
+      commonStyle: { color: 'white', fontSize: '30px', textAlign: 'center', padding: '10px 0' },
+      backgroundColor: 'green',
+      opacity: 0
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      const opacity = this.state.opacity + 0.01;
+      if (opacity > 1) {
+        this.setState({
+          opacity: 1
+        });
+        return;
+      }
+      this.setState({
+        opacity
+      });
+    }, 50);
+  }
+  componentWillUnmount() {
+    this.setState = () => false;
+  }
+  handleClick() {
+    const colorArr = ['green', 'purple', 'blue', 'red', 'cyan'];
+    const random = Math.floor(Math.random() * 5); // 0-4
+    console.log('random', random);
+    this.setState({
+      backgroundColor: colorArr[random],
+      opacity: 0
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button style={{ marginBottom: '20px', padding: '2px 10px' }} onClick={this.handleClick.bind(this)}>
+          点击
+        </button>
+        <div style={{ marginBottom: '20px' }} dangerouslySetInnerHTML={{ __html: this.state.content }}></div>
+        <div
+          style={{
+            ...this.state.commonStyle,
+            backgroundColor: this.state.backgroundColor,
+            opacity: this.state.opacity
+          }}>
+          大家好啊
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Style;
