a87577b1bd08b6597ae9c0df4f27509a749b01f6
steve zhang <871506263@qq.com>
2022-05-21 19:27:21 +0800

    提交代码于: 2022-05-21 19:27:21

--- src/Comment.js
+++ src/Comment.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './assets/style/card.css';
+import PropTypes from 'prop-types';
+console.log('PropTypes:', PropTypes);
+console.log('React.PropTypes:', React.PropTypes)
 
 class Comment extends Component {
+  static propTypes = { comment: Object };
   render() {
     console.log('Comment render this.props:', this.props);
     const { comment } = this.props;
-    console.log('comment:', comment)
     return (
       <div className='comment'>
         <div className='comment-user'>
