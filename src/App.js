b67f97cd92f808a6e2fa349ced0ba07897fc03fe
steve zhang <871506263@qq.com>
2022-05-28 20:47:51 +0800

    提交代码于: 2022-05-28 20:47:51

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import './assets/style/app.css';
-import Input from './Input';
-import Textarea from './Textarea';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <Input />
-        <Textarea />
+        hello world
       </div>
     );
   }
