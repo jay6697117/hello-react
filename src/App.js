c8817e4283ed2fa2f635bcb34ada6f12f229ae2f
steve zhang <871506263@qq.com>
2022-05-29 00:18:40 +0800

    提交代码于: 2022-05-29 00:18:40

--- src/App.js
+++ src/App.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 import './assets/style/app.css';
 import Header from './Header';
 import Main from './Main';
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  static prop;
+  // childContextTypes 作用跟 propTypes 验证组件 props 参数的作用类似
+  static childContextTypes = {
+    themeColor: PropTypes.string
+  };
+
+  constructor() {
+    super();
+    this.state = {
+      themeColor: 'red'
+    };
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      this.setState({
+        themeColor: 'blue'
+      });
+    }, 5000);
+  }
+
+  getChildContext() {
+    return { themeColor: this.state.themeColor };
+  }
+
   render() {
     return (
-      <div className="app">
+      <div className='app'>
         <Header />
         <Main />
       </div>
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends Component {
+  static contextTypes = {
+    themeColor: PropTypes.string
+  };
+
   render() {
-    return <h1>React.js 小书标题</h1>;
+    console.log('Title this.context:', this.context);
+    return <h1 style={{ color: this.context.themeColor }}>React.js 小书标题</h1>;
   }
 }
 
 class Header extends Component {
   render() {
     return (
-      <div className="header">
+      <div className='header'>
         <h2>This is header</h2>
         <Title />
       </div>
